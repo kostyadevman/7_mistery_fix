44b77ee1b03dea56e832a2ba5ebed6501e94d585
dolly <timohakt@yandex.ru>
2018-02-28 21:45:18 +0300

    change README

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -8,6 +8,6 @@ def get_roots(a, b, c):
     root1 = (-b - sqrt(discriminant)) / (2 * a)
     root2 = (-b + sqrt(discriminant)) / (2 * a)
     if discriminant == 0:
-        return root1, None 
+        return root1, None
     else:
         return root1, root2
